91c5acfb321e2444fc6e74e3b3c9f6ad78599b21
Larry Hudson <larryhudson@hey.com>
2021-11-20 10:38:16 +1100

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+const mammoth = require('mammoth')
+const cheerio = require('cheerio')
+const lodashMerge = require('lodash.merge')
+
+const globalOptions = {
+    njkLayout: 'layouts/docx.njk',
+    customTransform: null
+  };
+
+module.exports = function(eleventyConfig, configGlobalOptions = {}) {
+    // Return your Object options:
+    let options = lodashMerge({}, globalOptions, configGlobalOptions);
+
+    eleventyConfig.addTemplateFormats("docx");
+
+    eleventyConfig.addExtension('docx', {
+        getData: function() {
+            return {
+                layout: options.njkLayout
+            }
+        },
+        compile: function(str, inputPath) {
+            return async (data) => {
+
+                const rawHtml = await mammoth.convertToHtml({path: inputPath}).then(result => result.value)
+
+                if (options.customTransform) {
+                    const $ = cheerio.load(rawHtml)
+                    options.customTransform($)
+                    return $('body').html();
+                } else {
+                    return rawHtml
+                }
+            }
+        },
+        
+    })
+  };
